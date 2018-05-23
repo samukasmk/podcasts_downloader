a05eb89c1bdcb27bc819606f061f42460856e34c
samuel.sampaio <samuel.sampaio@tivit.com>
2018-05-23 00:52:36 -0300

    Reimplemented FilesPipeline as DownloadMP3FilesPipeline with correct file names

--- podcasts_downloader/pipelines.py
+++ podcasts_downloader/pipelines.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
 
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import os
+import re
+from scrapy.pipelines.files import FilesPipeline
+from podcasts_downloader.settings import FILES_STORE
+
+starts_with_id = re.compile('^[0-9]+')
 
 
-class PodcastsDownloaderPipeline(object):
-    def process_item(self, item, spider):
-        return item
+class DownloadMP3FilesPipeline(FilesPipeline):
+    def file_path(self, request, response=None, info=None):
+        # creates download folder if it does not exists
+        podcast_domain = request.url.split('/')[2]
+        os.makedirs(os.path.join(FILES_STORE, podcast_domain), exist_ok=True)
+
+        # get file name with postcast id if it does not have
+        podcast_file = request.url.split('/')[-1]
+        if not starts_with_id.match(podcast_file):
+            podcast_id = request.url.split('/')[-2]
+            podcast_file = f'{podcast_id}-{podcast_file}'
+
+        return os.path.join(podcast_domain, podcast_file)
